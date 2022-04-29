6c48b1e60bb1d439ef71ac5733dd2921edf575b9
leo1982 <leoscalesi@hotmail.com>
2022-04-28 21:55:37 -0300

    Version2. Se agregan clases de modelo de dominio.

---
+++ Dominio/Articulo.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Dominio
+{
+    public class Articulos
+    {
+        private int Id { get; set; }
+
+        private string Codigo { get; set; }
+
+        private string Nombre { get; set; }
+
+        private string Descripcion { get; set; }
+
+        private int Idmarca { get; set; }
+
+        private int IdCategoria { get; set; }
+
+        private string ImagenUrl { get; set; }
+
+        private float Precio  { get; set; }
+
+
+
+    }
+}
---
+++ Dominio/Marca.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Dominio
+{
+    public class Marca
+    {
+
+        private int Id { get; set; }
+
+        private string Descripcion { get; set; }
+
+    }
+
+
+
+
+}
