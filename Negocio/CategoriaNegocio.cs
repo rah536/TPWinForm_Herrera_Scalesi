7ee162c6c465f7fea03d80fd4e893c90bdaaae45
leo1982 <leoscalesi@hotmail.com>
2022-04-30 18:50:10 -0300

    no message

---
+++ Negocio/CategoriaNegocio.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Dominio;
+
+namespace Negocio
+{
+    public class CategoriaNegocio
+    {
+
+        public List<Categoria> listar()
+        {
+            List<Categoria> lista = new List<Categoria>();
+            AccesoDatos datos = new AccesoDatos();
+
+            try
+            {
+
+                datos.setearConsulta("select descripcion from categorias");
+                datos.ejecutarLectura();
+
+                while (datos.Lector.Read())
+                {
+
+                    Categoria aux = new Categoria();
+
+                    aux.Descripcion = (string)datos.Lector["descripcion"];
+                    
+                    lista.Add(aux);
+
+                }
+
+                return lista;
+            }
+
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+
+            finally
+            {
+                datos.cerrarConexion();
+            }
+
+        }
+
+
+
+
+    }
+
+
+
+}
---
+++ Negocio/MarcaNegocio.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Dominio;
+
+namespace Negocio
+{
+    public class MarcaNegocio
+    {
+
+        public List<Marca> listar()
+        {
+            List<Marca> lista = new List<Marca>();
+            AccesoDatos datos = new AccesoDatos();
+
+            try
+            {
+
+                datos.setearConsulta("select descripcion from marcas");
+                datos.ejecutarLectura();
+
+                while (datos.Lector.Read())
+                {
+
+                    Marca aux = new Marca();
+
+                    aux.Descripcion = (string)datos.Lector["descripcion"];
+
+                    lista.Add(aux);
+
+                }
+
+                return lista;
+            }
+
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+
+            finally
+            {
+                datos.cerrarConexion();
+            }
+        }
+
+    }
+}
