ec8a465ce2b7d7fb7ad599a187b529216565dfed
leo1982 <leoscalesi@hotmail.com>
2022-05-07 12:42:39 -0300

    Revert "Version21. Se agregan controles al Formulario Mostrar detalle."
    This reverts commit 2bc92f5dd2cf2b904091063fd014c729796de623.

--- TPWinForms/BusquedaArticulos.cs
+++ TPWinForms/BusquedaArticulos.cs
@@ -7,6 +7,8 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using Negocio;
+using Dominio;
 
 namespace TPWinForms
 {
@@ -16,5 +18,8 @@ namespace TPWinForms
         {
             InitializeComponent();
         }
+
+        
+
     }
 }
