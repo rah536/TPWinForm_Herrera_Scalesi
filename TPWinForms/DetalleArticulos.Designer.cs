ca7377d2d7e3c67ca3cdfcda676d4d7ee8b4d321
leo1982 <leoscalesi@hotmail.com>
2022-05-07 15:12:10 -0300

    Version22. Se agregan controles al Form Detalle Articulos.

--- TPWinForms/DetalleArticulos.Designer.cs
+++ TPWinForms/DetalleArticulos.Designer.cs
@@ -28,20 +28,299 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.lblDetalle = new System.Windows.Forms.Label();
+            this.lblIdArticulo = new System.Windows.Forms.Label();
+            this.lblNombreArticulo = new System.Windows.Forms.Label();
+            this.lblCodigo = new System.Windows.Forms.Label();
+            this.lblIdMarca = new System.Windows.Forms.Label();
+            this.lblMarca = new System.Windows.Forms.Label();
+            this.lblIdCategoria = new System.Windows.Forms.Label();
+            this.lblCategoria = new System.Windows.Forms.Label();
+            this.pbImagenUrl = new System.Windows.Forms.PictureBox();
+            this.lblDescripcion = new System.Windows.Forms.Label();
+            this.lblPrecio = new System.Windows.Forms.Label();
+            this.lblUrlImagen = new System.Windows.Forms.Label();
+            this.lblMuestraIdArticulo = new System.Windows.Forms.Label();
+            this.lblMuestraNombre = new System.Windows.Forms.Label();
+            this.lblMuestraCodigo = new System.Windows.Forms.Label();
+            this.lblMuestraIdMarca = new System.Windows.Forms.Label();
+            this.lblMuestraMarca = new System.Windows.Forms.Label();
+            this.lblMuestraIdCategoria = new System.Windows.Forms.Label();
+            this.lblMuestraCategoria = new System.Windows.Forms.Label();
+            this.lblMuestraPrecio = new System.Windows.Forms.Label();
+            this.lblMuestraUrlImagen = new System.Windows.Forms.Label();
+            this.lblMuestraDescripcion = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.pbImagenUrl)).BeginInit();
             this.SuspendLayout();
+            // 
+            // lblDetalle
+            // 
+            this.lblDetalle.AutoSize = true;
+            this.lblDetalle.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.lblDetalle.Font = new System.Drawing.Font("Tempus Sans ITC", 24F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblDetalle.Location = new System.Drawing.Point(295, 24);
+            this.lblDetalle.Name = "lblDetalle";
+            this.lblDetalle.Size = new System.Drawing.Size(155, 44);
+            this.lblDetalle.TabIndex = 0;
+            this.lblDetalle.Text = "DETALLE";
+            // 
+            // lblIdArticulo
+            // 
+            this.lblIdArticulo.AutoSize = true;
+            this.lblIdArticulo.Font = new System.Drawing.Font("MingLiU_HKSCS-ExtB", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblIdArticulo.Location = new System.Drawing.Point(46, 117);
+            this.lblIdArticulo.Name = "lblIdArticulo";
+            this.lblIdArticulo.Size = new System.Drawing.Size(115, 16);
+            this.lblIdArticulo.TabIndex = 1;
+            this.lblIdArticulo.Text = "ID ARTICULO:";
+            // 
+            // lblNombreArticulo
+            // 
+            this.lblNombreArticulo.AutoSize = true;
+            this.lblNombreArticulo.Font = new System.Drawing.Font("MingLiU_HKSCS-ExtB", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblNombreArticulo.Location = new System.Drawing.Point(46, 154);
+            this.lblNombreArticulo.Name = "lblNombreArticulo";
+            this.lblNombreArticulo.Size = new System.Drawing.Size(70, 16);
+            this.lblNombreArticulo.TabIndex = 2;
+            this.lblNombreArticulo.Text = "NOMBRE:";
+            // 
+            // lblCodigo
+            // 
+            this.lblCodigo.AutoSize = true;
+            this.lblCodigo.Font = new System.Drawing.Font("MingLiU_HKSCS-ExtB", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblCodigo.Location = new System.Drawing.Point(46, 197);
+            this.lblCodigo.Name = "lblCodigo";
+            this.lblCodigo.Size = new System.Drawing.Size(70, 16);
+            this.lblCodigo.TabIndex = 3;
+            this.lblCodigo.Text = "CODIGO:";
+            // 
+            // lblIdMarca
+            // 
+            this.lblIdMarca.AutoSize = true;
+            this.lblIdMarca.Font = new System.Drawing.Font("MingLiU_HKSCS-ExtB", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblIdMarca.Location = new System.Drawing.Point(46, 266);
+            this.lblIdMarca.Name = "lblIdMarca";
+            this.lblIdMarca.Size = new System.Drawing.Size(88, 16);
+            this.lblIdMarca.TabIndex = 4;
+            this.lblIdMarca.Text = "ID MARCA:";
+            // 
+            // lblMarca
+            // 
+            this.lblMarca.AutoSize = true;
+            this.lblMarca.Font = new System.Drawing.Font("MingLiU_HKSCS-ExtB", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblMarca.Location = new System.Drawing.Point(46, 314);
+            this.lblMarca.Name = "lblMarca";
+            this.lblMarca.Size = new System.Drawing.Size(61, 16);
+            this.lblMarca.TabIndex = 5;
+            this.lblMarca.Text = "MARCA:";
+            // 
+            // lblIdCategoria
+            // 
+            this.lblIdCategoria.AutoSize = true;
+            this.lblIdCategoria.Font = new System.Drawing.Font("MingLiU_HKSCS-ExtB", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblIdCategoria.Location = new System.Drawing.Point(46, 393);
+            this.lblIdCategoria.Name = "lblIdCategoria";
+            this.lblIdCategoria.Size = new System.Drawing.Size(124, 16);
+            this.lblIdCategoria.TabIndex = 6;
+            this.lblIdCategoria.Text = "ID CATEGORIA:";
+            // 
+            // lblCategoria
+            // 
+            this.lblCategoria.AutoSize = true;
+            this.lblCategoria.Font = new System.Drawing.Font("MingLiU_HKSCS-ExtB", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblCategoria.Location = new System.Drawing.Point(46, 431);
+            this.lblCategoria.Name = "lblCategoria";
+            this.lblCategoria.Size = new System.Drawing.Size(97, 16);
+            this.lblCategoria.TabIndex = 7;
+            this.lblCategoria.Text = "CATEGORIA:";
+            // 
+            // pbImagenUrl
+            // 
+            this.pbImagenUrl.Location = new System.Drawing.Point(332, 183);
+            this.pbImagenUrl.Name = "pbImagenUrl";
+            this.pbImagenUrl.Size = new System.Drawing.Size(198, 176);
+            this.pbImagenUrl.TabIndex = 8;
+            this.pbImagenUrl.TabStop = false;
+            // 
+            // lblDescripcion
+            // 
+            this.lblDescripcion.AutoSize = true;
+            this.lblDescripcion.Font = new System.Drawing.Font("MingLiU_HKSCS-ExtB", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblDescripcion.Location = new System.Drawing.Point(329, 117);
+            this.lblDescripcion.Name = "lblDescripcion";
+            this.lblDescripcion.Size = new System.Drawing.Size(115, 16);
+            this.lblDescripcion.TabIndex = 9;
+            this.lblDescripcion.Text = "DESCRIPCION:";
+            // 
+            // lblPrecio
+            // 
+            this.lblPrecio.AutoSize = true;
+            this.lblPrecio.Font = new System.Drawing.Font("MingLiU_HKSCS-ExtB", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblPrecio.Location = new System.Drawing.Point(332, 393);
+            this.lblPrecio.Name = "lblPrecio";
+            this.lblPrecio.Size = new System.Drawing.Size(70, 16);
+            this.lblPrecio.TabIndex = 10;
+            this.lblPrecio.Text = "PRECIO:";
+            // 
+            // lblUrlImagen
+            // 
+            this.lblUrlImagen.AutoSize = true;
+            this.lblUrlImagen.Font = new System.Drawing.Font("MingLiU_HKSCS-ExtB", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblUrlImagen.Location = new System.Drawing.Point(332, 433);
+            this.lblUrlImagen.Name = "lblUrlImagen";
+            this.lblUrlImagen.Size = new System.Drawing.Size(106, 16);
+            this.lblUrlImagen.TabIndex = 11;
+            this.lblUrlImagen.Text = "URL IMAGEN:";
+            // 
+            // lblMuestraIdArticulo
+            // 
+            this.lblMuestraIdArticulo.AutoSize = true;
+            this.lblMuestraIdArticulo.Location = new System.Drawing.Point(199, 117);
+            this.lblMuestraIdArticulo.Name = "lblMuestraIdArticulo";
+            this.lblMuestraIdArticulo.Size = new System.Drawing.Size(35, 13);
+            this.lblMuestraIdArticulo.TabIndex = 12;
+            this.lblMuestraIdArticulo.Text = "label1";
+            // 
+            // lblMuestraNombre
+            // 
+            this.lblMuestraNombre.AutoSize = true;
+            this.lblMuestraNombre.Location = new System.Drawing.Point(199, 157);
+            this.lblMuestraNombre.Name = "lblMuestraNombre";
+            this.lblMuestraNombre.Size = new System.Drawing.Size(35, 13);
+            this.lblMuestraNombre.TabIndex = 13;
+            this.lblMuestraNombre.Text = "label2";
+            // 
+            // lblMuestraCodigo
+            // 
+            this.lblMuestraCodigo.AutoSize = true;
+            this.lblMuestraCodigo.Location = new System.Drawing.Point(199, 200);
+            this.lblMuestraCodigo.Name = "lblMuestraCodigo";
+            this.lblMuestraCodigo.Size = new System.Drawing.Size(35, 13);
+            this.lblMuestraCodigo.TabIndex = 14;
+            this.lblMuestraCodigo.Text = "label3";
+            // 
+            // lblMuestraIdMarca
+            // 
+            this.lblMuestraIdMarca.AutoSize = true;
+            this.lblMuestraIdMarca.Location = new System.Drawing.Point(199, 268);
+            this.lblMuestraIdMarca.Name = "lblMuestraIdMarca";
+            this.lblMuestraIdMarca.Size = new System.Drawing.Size(35, 13);
+            this.lblMuestraIdMarca.TabIndex = 15;
+            this.lblMuestraIdMarca.Text = "label4";
+            // 
+            // lblMuestraMarca
+            // 
+            this.lblMuestraMarca.AutoSize = true;
+            this.lblMuestraMarca.Location = new System.Drawing.Point(199, 316);
+            this.lblMuestraMarca.Name = "lblMuestraMarca";
+            this.lblMuestraMarca.Size = new System.Drawing.Size(35, 13);
+            this.lblMuestraMarca.TabIndex = 16;
+            this.lblMuestraMarca.Text = "label5";
+            // 
+            // lblMuestraIdCategoria
+            // 
+            this.lblMuestraIdCategoria.AutoSize = true;
+            this.lblMuestraIdCategoria.Location = new System.Drawing.Point(199, 393);
+            this.lblMuestraIdCategoria.Name = "lblMuestraIdCategoria";
+            this.lblMuestraIdCategoria.Size = new System.Drawing.Size(35, 13);
+            this.lblMuestraIdCategoria.TabIndex = 17;
+            this.lblMuestraIdCategoria.Text = "label6";
+            // 
+            // lblMuestraCategoria
+            // 
+            this.lblMuestraCategoria.AutoSize = true;
+            this.lblMuestraCategoria.Location = new System.Drawing.Point(199, 436);
+            this.lblMuestraCategoria.Name = "lblMuestraCategoria";
+            this.lblMuestraCategoria.Size = new System.Drawing.Size(35, 13);
+            this.lblMuestraCategoria.TabIndex = 18;
+            this.lblMuestraCategoria.Text = "label7";
+            // 
+            // lblMuestraPrecio
+            // 
+            this.lblMuestraPrecio.AutoSize = true;
+            this.lblMuestraPrecio.Location = new System.Drawing.Point(445, 395);
+            this.lblMuestraPrecio.Name = "lblMuestraPrecio";
+            this.lblMuestraPrecio.Size = new System.Drawing.Size(35, 13);
+            this.lblMuestraPrecio.TabIndex = 19;
+            this.lblMuestraPrecio.Text = "label8";
+            // 
+            // lblMuestraUrlImagen
+            // 
+            this.lblMuestraUrlImagen.AutoSize = true;
+            this.lblMuestraUrlImagen.Location = new System.Drawing.Point(445, 434);
+            this.lblMuestraUrlImagen.Name = "lblMuestraUrlImagen";
+            this.lblMuestraUrlImagen.Size = new System.Drawing.Size(35, 13);
+            this.lblMuestraUrlImagen.TabIndex = 20;
+            this.lblMuestraUrlImagen.Text = "label9";
+            // 
+            // lblMuestraDescripcion
+            // 
+            this.lblMuestraDescripcion.AutoSize = true;
+            this.lblMuestraDescripcion.Location = new System.Drawing.Point(332, 156);
+            this.lblMuestraDescripcion.Name = "lblMuestraDescripcion";
+            this.lblMuestraDescripcion.Size = new System.Drawing.Size(35, 13);
+            this.lblMuestraDescripcion.TabIndex = 21;
+            this.lblMuestraDescripcion.Text = "label1";
             // 
             // DetalleArticulos
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.SystemColors.ActiveCaption;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(800, 499);
+            this.Controls.Add(this.lblMuestraDescripcion);
+            this.Controls.Add(this.lblMuestraUrlImagen);
+            this.Controls.Add(this.lblMuestraPrecio);
+            this.Controls.Add(this.lblMuestraCategoria);
+            this.Controls.Add(this.lblMuestraIdCategoria);
+            this.Controls.Add(this.lblMuestraMarca);
+            this.Controls.Add(this.lblMuestraIdMarca);
+            this.Controls.Add(this.lblMuestraCodigo);
+            this.Controls.Add(this.lblMuestraNombre);
+            this.Controls.Add(this.lblMuestraIdArticulo);
+            this.Controls.Add(this.lblUrlImagen);
+            this.Controls.Add(this.lblPrecio);
+            this.Controls.Add(this.lblDescripcion);
+            this.Controls.Add(this.pbImagenUrl);
+            this.Controls.Add(this.lblCategoria);
+            this.Controls.Add(this.lblIdCategoria);
+            this.Controls.Add(this.lblMarca);
+            this.Controls.Add(this.lblIdMarca);
+            this.Controls.Add(this.lblCodigo);
+            this.Controls.Add(this.lblNombreArticulo);
+            this.Controls.Add(this.lblIdArticulo);
+            this.Controls.Add(this.lblDetalle);
             this.Name = "DetalleArticulos";
-            this.Text = "DetalleArticulos";
+            this.Text = "Detalle Articulos";
+            ((System.ComponentModel.ISupportInitialize)(this.pbImagenUrl)).EndInit();
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
+
+        private System.Windows.Forms.Label lblDetalle;
+        private System.Windows.Forms.Label lblIdArticulo;
+        private System.Windows.Forms.Label lblNombreArticulo;
+        private System.Windows.Forms.Label lblCodigo;
+        private System.Windows.Forms.Label lblIdMarca;
+        private System.Windows.Forms.Label lblMarca;
+        private System.Windows.Forms.Label lblIdCategoria;
+        private System.Windows.Forms.Label lblCategoria;
+        private System.Windows.Forms.PictureBox pbImagenUrl;
+        private System.Windows.Forms.Label lblDescripcion;
+        private System.Windows.Forms.Label lblPrecio;
+        private System.Windows.Forms.Label lblUrlImagen;
+        private System.Windows.Forms.Label lblMuestraIdArticulo;
+        private System.Windows.Forms.Label lblMuestraNombre;
+        private System.Windows.Forms.Label lblMuestraCodigo;
+        private System.Windows.Forms.Label lblMuestraIdMarca;
+        private System.Windows.Forms.Label lblMuestraMarca;
+        private System.Windows.Forms.Label lblMuestraIdCategoria;
+        private System.Windows.Forms.Label lblMuestraCategoria;
+        private System.Windows.Forms.Label lblMuestraPrecio;
+        private System.Windows.Forms.Label lblMuestraUrlImagen;
+        private System.Windows.Forms.Label lblMuestraDescripcion;
     }
 }
